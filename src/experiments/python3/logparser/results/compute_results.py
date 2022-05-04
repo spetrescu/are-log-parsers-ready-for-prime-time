9b0a6df873bff50d2cd28986b0b7f882b1307f0b
Stefan Petrescu <stefan.petrescu997@gmail.com>
2022-05-04 21:46:13 +0200

    Update accuracy compute for Python 3 experiments

--- src/experiments/python3/logparser/results/compute_results.py
+++ src/experiments/python3/logparser/results/compute_results.py
@@ -32,8 +32,11 @@ def compute_accuracy(ground_truth, parsed_result):
     ground_truth_df = pd.read_csv(ground_truth)
     parsed_result_df = pd.read_csv(parsed_result)
 
-    ground_truth_df = ground_truth_df["EventTemplate"]
-    parsed_result_df = parsed_result_df["EventTemplate"]
+    ground_truth_df = ground_truth_df[["EventTemplate"]]
+    parsed_result_df = parsed_result_df[["EventTemplate"]]
+
+    if METHOD == "MoLFI":
+        parsed_result_df['EventTemplate'] = parsed_result_df['EventTemplate'].str.replace('#spec#', '<*>')
 
     return accuracy_score(ground_truth_df, parsed_result_df)
 
@@ -78,7 +81,7 @@ for dset in dsets:
         #     groundtruth=groundtruth, parsedresult=parsedresult
         # )
         accuracy = compute_accuracy(ground_truth=groundtruth, parsed_result=parsedresult)
-        print("Accuracy is: ", accuracy)
+        print("Accuracy is: ", accuracy, "for {0}".format(parsed_log_file))
 
         #results.append([dset, precision, reacall, F1_measure, accuracy])
         results.append([dset, accuracy, accuracy, accuracy, accuracy])
